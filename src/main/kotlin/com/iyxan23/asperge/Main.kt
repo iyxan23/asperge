3afa12f9c9942c3dc5f99c3df72152c6a05479a1
Iyxan23 <nurihsanalghifari@gmail.com>
2021-07-02 19:15:39 +0700

    feat: Parse and print files on Main

--- src/main/kotlin/com/iyxan23/asperge/Main.kt
+++ src/main/kotlin/com/iyxan23/asperge/Main.kt
@@ -1,7 +1,14 @@
 package com.iyxan23.asperge
 
+import com.iyxan23.asperge.sketchware.FileParser
+import com.iyxan23.asperge.sketchware.LogicParser
+import com.iyxan23.asperge.sketchware.ViewParser
 import com.iyxan23.asperge.unpacker.Unpacker
 
 fun main(args: Array<String>) {
-    println(Unpacker.unpack(args[0]))
+    val unpacked = Unpacker.unpack(args[0])
+    println(unpacked)
+    println(LogicParser(unpacked.logic).parse())
+    println(ViewParser(unpacked.view).parse())
+    println(FileParser(unpacked.file).parse())
 }
