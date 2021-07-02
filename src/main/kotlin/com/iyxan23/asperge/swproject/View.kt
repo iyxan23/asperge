f3090637e3d67071cbd6f71acaea5fc40ef5e0ab
Iyxan23 <nurihsanalghifari@gmail.com>
2021-07-02 10:50:25 +0700

    Partially create model class View

---
+++ src/main/kotlin/com/iyxan23/asperge/swproject/View.kt
@@ -0,0 +1,7 @@
+package com.iyxan23.asperge.swproject
+
+import com.iyxan23.asperge.swproject.view.ViewSection
+
+data class View(
+    val sections: List<ViewSection>
+)
---
+++ src/main/kotlin/com/iyxan23/asperge/swproject/view/ViewSection.kt
@@ -0,0 +1,8 @@
+package com.iyxan23.asperge.swproject.view
+
+data class ViewSection(
+    val name: String,
+    val ext: String,
+
+    val views: List<ViewItem>
+)
