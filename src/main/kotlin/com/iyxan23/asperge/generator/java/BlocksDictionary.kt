7c673544769e2fc9d1cff975c8eb0ee54c9ed329
Iyxan23 <nurihsanalghifari@gmail.com>
2021-07-04 05:17:56 +0700

    chore: Optimize BlocksDictionary with adding semicolon after the when statement

--- src/main/kotlin/com/iyxan23/asperge/generator/java/BlocksDictionary.kt
+++ src/main/kotlin/com/iyxan23/asperge/generator/java/BlocksDictionary.kt
@@ -3,21 +3,22 @@ package com.iyxan23.asperge.generator.java
 object BlocksDictionary {
     fun generateCode(opCode: String, parameters: List<String>, spec: String, addSemicolon: Boolean = true): String {
         println("$opCode: $parameters")
-        return when (opCode) {
-            "getVar" -> spec                                                                                            + if (addSemicolon) ";" else ""
-            "toString" -> "${parameters[0]}.toString()"                                                                 + if (addSemicolon) ";" else ""
 
-            "setText" -> "${parameters[0]}.setText(${parameters[1]})"                                                   + if (addSemicolon) ";" else ""
+        if (opCode == "addSourceDirectly") return parameters[0]
 
-            "setVarInt" -> "${parameters[0]} = ${parameters[1]}"                                                        + if (addSemicolon) ";" else ""
-            "increaseInt" -> "${parameters[0]}++"                                                                       + if (addSemicolon) ";" else ""
+        return (when (opCode) {
+            "getVar" -> spec
+            "toString" -> "${parameters[0]}.toString()"
 
-            "addSourceDirectly" -> parameters[0]
-            "finishActivity" -> "finishActivity()"                                                                      + if (addSemicolon) ";" else ""
+            "setText" -> "${parameters[0]}.setText(${parameters[1]})"
 
-            else -> {
-                "Unknown"
-            }
-        }
+            "setVarInt" -> "${parameters[0]} = ${parameters[1]}"
+            "increaseInt" -> "${parameters[0]}++"
+
+            "finishActivity" -> "finishActivity()"
+
+            else -> "Unknown opcode $opCode"
+
+        }) + if (addSemicolon) ";" else ""
     }
 }
