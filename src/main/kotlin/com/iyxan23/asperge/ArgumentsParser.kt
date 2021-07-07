33251c26c2aecc81a0b336852da0150bc8ad8e1b
Iyxan23 <nurihsanalghifari@gmail.com>
2021-07-07 11:57:05 +0700

    feat: Implement ArgumentParser

--- src/main/kotlin/com/iyxan23/asperge/ArgumentsParser.kt
+++ src/main/kotlin/com/iyxan23/asperge/ArgumentsParser.kt
@@ -1,4 +1,31 @@
 package com.iyxan23.asperge
 
-object ArgumentsParser {
+class ArgumentsParser(
+    private val flags: List<String> = emptyList(),
+    private val strings: List<String> = emptyList(),
+) {
+    private var captureNext = false
+    private var lastStringFlag = ""
+
+    //                                       flag    string value
+    fun parse(args: List<String>): List<Pair<String, String?>> {
+        return args.mapNotNull {
+            when {
+                captureNext -> {
+                    captureNext = false
+                    Pair(it, lastStringFlag)
+                }
+
+                flags.contains(it) -> Pair(it, null)
+
+                strings.contains(it) -> {
+                    captureNext = true
+                    lastStringFlag = it
+                    Pair(it, null)
+                }
+
+                else -> null
+            }
+        }
+    }
 }
